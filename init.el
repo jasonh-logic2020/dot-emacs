d02f662d0e57ad2e0d72fb0df9f5f3cec85aec4a
jasonh <jasonh@logic2020.com>
2020-02-25 13:30:58 -0800

    Bind dumb-jump-back

--- init.el
+++ init.el
@@ -2913,6 +2913,7 @@ If region is active, apply to active region instead."
   ;; BULK-ENSURE :ensure t
   :bind (("M-g o" . dumb-jump-go-other-window))
   ("M-g j" . dumb-jump-go)
+  ("M-g b" . dumb-jump-back)
   ("M-g x" . dumb-jump-go-prefer-external)
   ("M-g z" . dumb-jump-go-prefer-external-other-window)
   :config
